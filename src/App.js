df21ae2c67b305f627be9d333498f41ecbe5deb6
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-10-19 18:25:53 +0300

    Scroll to top added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import useSmoothScroll from "./hooks/useSmoothScroll";
 
 function App() {
   const [refToScroll, smoothScroll] = useSmoothScroll();
+  const [topRef, smoothScrollToTop] = useSmoothScroll();
   return (
     <div className="App">
-      <button onClick={smoothScroll}>Scroll</button>
+      <button ref={topRef} onClick={smoothScroll}>
+        Scroll
+      </button>
       <div
         style={{ marginTop: "150vh", marginBottom: "20vh" }}
         ref={refToScroll}
       >
         I wanna be seen
       </div>
+      <button onClick={smoothScrollToTop}>Go UP</button>
     </div>
   );
 }
