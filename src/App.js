90c0339d9ebb40d462e9ae133204a971e8b5a217
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-10-19 18:22:47 +0300

    useSmoothScroll hook added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useSmoothScroll from "./hooks/useSmoothScroll";
 
 function App() {
+  const [refToScroll, smoothScroll] = useSmoothScroll();
   return (
     <div className="App">
-      
+      <button onClick={smoothScroll}>Scroll</button>
+      <div
+        style={{ marginTop: "150vh", marginBottom: "20vh" }}
+        ref={refToScroll}
+      >
+        I wanna be seen
+      </div>
     </div>
   );
 }
